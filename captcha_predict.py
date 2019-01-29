4dd223fcce2285aed55e22b4bb383a3a5d1e421b
Tony Ju <tony.ju@me.com>
2019-01-29 21:20:56 +0800

    show predict percentage

--- captcha_predict.py
+++ captcha_predict.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
+from os import listdir
+
 import numpy as np
 import torch
 from torch.autograd import Variable
-#from visdom import Visdom # pip install Visdom
+
+# from visdom import Visdom # pip install Visdom
 import captcha_setting
 import my_dataset
 from captcha_cnn_model import CNN
+
 
 def main():
     cnn = CNN()
@@ -15,22 +19,50 @@ def main():
 
     predict_dataloader = my_dataset.get_predict_data_loader()
 
-    #vis = Visdom()
+    # vis = Visdom()
+
+    results = []
     for i, (images, labels) in enumerate(predict_dataloader):
         image = images
         vimage = Variable(image)
         predict_label = cnn(vimage)
 
         c0 = captcha_setting.ALL_CHAR_SET[np.argmax(predict_label[0, 0:captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
-        c1 = captcha_setting.ALL_CHAR_SET[np.argmax(predict_label[0, captcha_setting.ALL_CHAR_SET_LEN:2 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
-        c2 = captcha_setting.ALL_CHAR_SET[np.argmax(predict_label[0, 2 * captcha_setting.ALL_CHAR_SET_LEN:3 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
-        c3 = captcha_setting.ALL_CHAR_SET[np.argmax(predict_label[0, 3 * captcha_setting.ALL_CHAR_SET_LEN:4 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
+        c1 = captcha_setting.ALL_CHAR_SET[np.argmax(
+            predict_label[0, captcha_setting.ALL_CHAR_SET_LEN:2 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
+        c2 = captcha_setting.ALL_CHAR_SET[np.argmax(
+            predict_label[0, 2 * captcha_setting.ALL_CHAR_SET_LEN:3 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
+        c3 = captcha_setting.ALL_CHAR_SET[np.argmax(
+            predict_label[0, 3 * captcha_setting.ALL_CHAR_SET_LEN:4 * captcha_setting.ALL_CHAR_SET_LEN].data.numpy())]
 
         c = '%s%s%s%s' % (c0, c1, c2, c3)
         print(c)
-        #vis.images(image, opts=dict(caption=c))
+        results.append(c)
+
+        # vis.images(image, opts=dict(caption=c))
+    codes = get_file_codes()
+    hits = get_result(codes, results)
+
+    print float(len(hits)) / float(len(codes)) * 100
+
+
+def get_result(codes, results):
+    hits = []
+    for code in codes:
+        for item in results:
+            if code == item:
+                hits.append(code)
+    return hits
+
+
+def get_file_codes():
+    codes = []
+    files = listdir(captcha_setting.PREDICT_DATASET_PATH)
+    for item in files:
+        code = item.split("_")[0]
+        codes.append(code)
+    return codes
+
 
 if __name__ == '__main__':
     main()
-
-
